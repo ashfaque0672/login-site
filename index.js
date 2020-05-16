585dc8ff37a6b76393500623b033df0a94343a21
ashfaque0672 <64634459+ashfaque0672@users.noreply.github.com>
2020-05-17 02:19:10 +0530

    my

---
+++ index.js
@@ -0,0 +1,15 @@
+function validate()
+{
+    var username = document.getElementById("uname");
+    var password = document.getElementById("pass");
+
+    if(username.value.trim()==""||password.value.trim()=="")
+    {
+        alert("No blank values allowed");
+        return false;
+    }
+    else 
+    {
+     true;
+    }
+}
