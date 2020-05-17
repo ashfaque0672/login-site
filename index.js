0f0153a11869414eac6ad22ef80ae3ddc2646064
ashfaque0672 <64634459+ashfaque0672@users.noreply.github.com>
2020-05-17 12:23:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+  
+   console.log('hello Worl w so times');
+   let name = 'Ashfaque';
+   const pi=3.14;
+   console.log(name);
+   console.log(pi);
+
+   //primitive data type
+   let isTrue = false; //boolean literal
+   let selectColor = 'red';
+   console.log(selectColor);
+   selectColor = null;
+   console.log(selectColor);
+   //console.log(isTrue);
+
+//object 
+//Dot notation
+   let person = {
+    class1 :12,
+    section3:'d'
+
+   }
+   console.log(person);
+   console.log(person.class1);
+
+   //Bracket Notation
+   person['class1']=329;
+   console.log(person.class1);
